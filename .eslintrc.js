4966a1858fbb2678ec275e144850f041d73e9441
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-07-18 20:31:54 +0200

    add eslint / prettier and tailwind configuration

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "tailwindcss"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
