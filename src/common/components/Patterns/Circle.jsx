e845d7bb378fba3af7abcd15cb50d42bbf5aceac
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-07-23 17:52:45 +0200

    add all svg patterns as components, rename RightArrow to RightDownwardArrow

---
+++ src/common/components/Patterns/Circle.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import {
+  opacityVariantsShape,
+  pathVariantsShape,
+} from "/src/common/utils/animations";
+
+function Circle(props) {
+  const { className, svgVariants, pathVariants } = props;
+
+  return (
+    <motion.svg
+      initial="initial"
+      animate="animate"
+      variants={svgVariants}
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="445"
+      height="445"
+    >
+      <motion.circle
+        variants={pathVariants}
+        cx="302.5"
+        cy="222.5"
+        r="222.5"
+        fill="#E5ECF4"
+        fillRule="evenodd"
+        transform="translate(-80)"
+      />
+    </motion.svg>
+  );
+}
+
+Circle.propTypes = {
+  className: PropTypes.string,
+  svgVariants: opacityVariantsShape,
+  pathVariants: pathVariantsShape,
+};
+
+Circle.defaultProps = {
+  className: "",
+  svgVariants: {},
+  pathVariants: {},
+};
+
+export default Circle;
