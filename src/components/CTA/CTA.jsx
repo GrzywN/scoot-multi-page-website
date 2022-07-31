13c533248e30036efe4c838b39795d8d63419355
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-07-31 17:33:21 +0200

    fix scrolling into download section/into view

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -7,12 +7,15 @@ function CTA(props) {
 
   return (
     <Link
-      onClick={onClick}
+      onClick={() => {
+        if (to.charAt(0) === "#") document.querySelector(to).scrollIntoView();
+        onClick();
+      }}
       to={to}
       className={`
       ${className}
       grid h-[3.3125rem] w-full place-items-center border-[0.1875rem] border-yellow bg-yellow
-        font-monospace text-15 font-bold text-white transition-colors duration-400 ease-in-out
+        font-monospace text-15 font-bold text-white transition-colors duration-500 ease-in-out
           hover:bg-transparent hover:text-yellow focus-visible:bg-transparent focus-visible:text-yellow
       md:max-w-[11.25rem]`}
     >
